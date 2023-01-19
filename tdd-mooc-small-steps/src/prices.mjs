07074fd03e8944d501a59ad43b86dfa70bcb58c7
rhiskk <hiski.r@gmail.com>
2023-01-19 12:23:35 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -30,6 +30,7 @@ function createApp(database) {
   }
 
   function parseTemporalDate(dateString) { };
+
   function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
