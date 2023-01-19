2a64f10d28137c0634fcce927a44111af85144fb
rhiskk <hiski.r@gmail.com>
2023-01-19 12:53:58 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -18,7 +18,6 @@ function createApp(database) {
     const age = req.query.age;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parseDate(req.query.date);
     const temporalDate = parseTemporalDate(req.query.date);
     const cost = calculateCost(age, type, temporalDate, baseCost);
     res.json({ cost });
