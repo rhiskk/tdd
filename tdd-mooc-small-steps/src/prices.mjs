b566aaaf23a4113d033117336ae07ed32817578f
rhiskk <hiski.r@gmail.com>
2023-01-19 12:54:30 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -23,8 +23,6 @@ function createApp(database) {
     res.json({ cost });
   });
 
-
-
   function parseTemporalDate(dateString) {
     if (dateString) {
       return Temporal.PlainDate.from(dateString);
