ef08ded94d34b02057fd28de69adac7b5857f1e6
rhiskk <hiski.r@gmail.com>
2023-01-23 16:27:41 +0200

    block stops when it hits the bottom

--- tetris/src/Board.mjs
+++ tetris/src/Board.mjs
@@ -1,42 +1,56 @@
 export class Board {
   width;
   height;
-  block;
+  fallingBlock;
+  stationaryBlocks;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.block = null
+    this.fallingBlock = null
+    this.stationaryBlocks = Array(height).fill().map(() => Array(width).fill("."));
   }
 
   toString() {
     let board = "";
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        if (this.block && i === this.block.row && j === this.block.column) {
-          board += this.block.color;
-        } else {
-        board += ".";
-        }
+        board += this.blockAt(i, j);
       }
       board += "\n";
     }
     return board;
   }
 
+  blockAt(row, column) {
+    if (this.hasFallingAt(row, column)) {
+      return this.fallingBlock.color;
+    }
+    return this.stationaryBlocks[row][column];
+  }
+
+  hasFallingAt(row, column) {
+    return this.hasFalling() && this.fallingBlock.row === row && this.fallingBlock.column === column;
+  }
+
   drop(block) {
-    if (this.block) {
+    if (this.hasFalling()) {
       throw new Error("already falling");
     }
-    this.block = block;
+    this.fallingBlock = block;
   }
 
   tick() {
-    this.block.tick();
+    if (this.fallingBlock.row === this.height - 1) {
+      this.stationaryBlocks[this.fallingBlock.row][this.fallingBlock.column] = this.fallingBlock.color;
+      this.fallingBlock = null;
+    } else {
+      this.fallingBlock.tick();
+    }
   }
 
   hasFalling() {
-    return this.block !== null;
+    return this.fallingBlock !== null;
   }
 
 }
