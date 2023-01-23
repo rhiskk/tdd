78697cfdc99b899ef8ec82bf4b0891c483e212d7
rhiskk <hiski.r@gmail.com>
2023-01-23 15:33:15 +0200

    The board starts empty

--- tetris/src/Board.mjs
+++ tetris/src/Board.mjs
@@ -8,6 +8,13 @@ export class Board {
   }
 
   toString() {
-    return "TODO";
+    let board = "";
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        board += ".";
+      }
+      board += "\n";
+    }
+    return board;
   }
 }
