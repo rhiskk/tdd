68f1b13e4021d95a320b7a853bed0f0cb4032888
rhiskk <hiski.r@gmail.com>
2023-01-23 15:42:25 +0200

    Block start from the top middle

--- tetris/src/Board.mjs
+++ tetris/src/Board.mjs
@@ -11,10 +11,19 @@ export class Board {
     let board = "";
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
+        if (this.block && i === 0 && j === 1) {
+          board += this.block.color;
+        } else {
         board += ".";
+        }
       }
       board += "\n";
     }
     return board;
   }
+
+  drop(block) {
+    this.block = block;
+  }
+
 }
