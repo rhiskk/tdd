8e494d0f050ec4d06bcf6a34c4e01d2209d51957
rhiskk <hiski.r@gmail.com>
2023-03-03 09:03:28 +0200

    todo form

--- todo/src/components/TodoForm.tsx
+++ todo/src/components/TodoForm.tsx
@@ -1,7 +1,22 @@
-export const TodoForm = () => {
+import { useState } from 'react'
+
+export const TodoForm = ({ addTodo }: any) => {
+  const [title, setTitle] = useState('')
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value)
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    addTodo(title)
+    setTitle('')
+  }
+
   return (
-    <form>
-      <input id="title" type="text" />
+    <form onSubmit={handleSubmit}>
+      <input id="title" type="text" value={title} onChange={onChange} />
+      <button type="submit">Add Todo</button>
     </form>
   )
 }
--- todo/test/ui/TodoForm.test.tsx
+++ todo/test/ui/TodoForm.test.tsx
@@ -1,10 +1,32 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { TodoForm } from '@/components/TodoForm'
 
 describe('TodoForm', () => {
   it('should render an input field for title', () => {
     render(<TodoForm />)
-    const titleInput = screen.getByRole('textbox', { id: /title/i })
+    const titleInput = screen.getByRole('textbox')
     expect(titleInput).toBeInTheDocument()
+  })
+
+  it('should render a submit button', () => {
+    render(<TodoForm />)
+    const submitButton = screen.getByRole('button')
+    expect(submitButton).toBeInTheDocument()
+  })
+
+  it('should call addTodo with input when the submit button is clicked', async () => {
+    const user = userEvent.setup()
+    const mockAddTodo = jest.fn()
+
+    render(<TodoForm addTodo={mockAddTodo} />)
+
+    const submitButton = screen.getByRole('button')
+    const input = screen.getByRole('textbox')
+
+    await user.type(input, 'Todo 1')
+    await user.click(submitButton)
+
+    expect(mockAddTodo).toHaveBeenCalledWith('Todo 1')
   })
 })
