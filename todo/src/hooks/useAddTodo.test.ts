2bd65228c03fe2d7bdcce725615a385a214eb799
rhiskk <hiski.r@gmail.com>
2023-03-07 15:29:06 +0200

    add todo hook

--- todo/src/hooks/useAddTodo.test.ts
+++ todo/src/hooks/useAddTodo.test.ts
@@ -1,21 +1,19 @@
-import { renderHook } from '@testing-library/react'
+import { renderHook, act } from '@testing-library/react'
 import useAddTodo from './useAddTodo'
 
 describe('useAddTodo', () => {
   it('should send a post request with the title and return a created todo', async () => {
-    const { result } = renderHook(useAddTodo, {
-      initialProps: {
-        title: 'Todo test',
-      },
+    const { result } = renderHook(useAddTodo)
+
+    await act(async () => {
+      await result.current.addTodo('Todo test')
     })
 
-    expect(result.current.newTodo).toEqual([
-      {
-        id: '3',
-        title: 'Todo test',
-        completed: false,
-        archived: false,
-      },
-    ])
+    expect(result.current.newTodo).toEqual({
+      id: '3',
+      title: 'Todo test',
+      completed: false,
+      archived: false,
+    })
   })
 })
--- todo/src/hooks/useAddTodo.ts
+++ todo/src/hooks/useAddTodo.ts
@@ -1,1 +1,20 @@
-export default function useAddTodo() {}
+import { useState } from 'react'
+import { Todo } from '../types'
+
+export default function useAddTodo() {
+  const [newTodo, setNewTodo] = useState<Todo>()
+
+  const addTodo = async (title: string) => {
+    const response = await fetch('/api/todo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title }),
+    })
+    const todo = await response.json()
+    setNewTodo(todo)
+  }
+
+  return { newTodo, addTodo }
+}
